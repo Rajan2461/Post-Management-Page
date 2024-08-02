a8939b7cac56c7ca1d2be369ea66e7c7939e1f14
rajan <138423034+Rajan2461@users.noreply.github.com>
2024-08-02 10:51:31 +0530

    Update posts.component.ts

--- src/app/posts/posts.component.ts
+++ src/app/posts/posts.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
-import { PostService } from '../post.service';
-import { NotificationComponent } from '../notification/notification.component';
+
 
 @Component({
   selector: 'app-posts',
@@ -8,42 +7,5 @@ import { NotificationComponent } from '../notification/notification.component';
   styleUrls: ['./posts.component.css']
 })
 export class PostsComponent implements OnInit {
-  posts: any[] = [];
-  editingPostId: number | null = null;
-  postContent: string = '';
 
-  @ViewChild('notification') notification: NotificationComponent | undefined;
-
-  constructor(private postService: PostService) { }
-
-  ngOnInit(): void {
-    this.fetchPosts();
-  }
-
-  fetchPosts(): void {
-    this.postService.getPosts().subscribe((posts) => {
-      this.posts = posts;
-    });
-  }
-
-  updatePost(post: any, notification: NotificationComponent): void {
-    if (this.editingPostId !== null) {
-      this.postService.updatePost(this.editingPostId, { ...post, body: this.postContent }).subscribe(() => {
-        this.fetchPosts();
-        this.editingPostId = null;
-        this.postContent = '';
-        notification.showNotification('Post updated successfully', 'success');
-      });
-    } else {
-      this.editingPostId = post.id;
-      this.postContent = post.body;
-    }
-  }
-
-  deletePost(id: number, notification: NotificationComponent): void {
-    this.postService.deletePost(id).subscribe(() => {
-      this.fetchPosts();
-      notification.showNotification('Post deleted successfully', 'success');
-    });
-  }
 }
